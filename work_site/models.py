7375854b59f138675dab0645a7fbc0285c682239
Sergey Grigorev <grigoscool@mail.ru>
2023-02-01 21:11:48 +0300

    1

--- work_site/models.py
+++ work_site/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+class AbstractEmploy(models.Model):
+    fio = models.CharField(max_length=255)
+    job = models.CharField(max_length=255)
+    employment_date = models.DateTimeField(auto_created=True, blank=True)
+    salary = models.IntegerField()
+
+    def __str__(self):
+        return self.fio
+    class Meta:
+        abstract = True
+
+
+class Directors(AbstractEmploy):
+
+class AssociateDirs(AbstractEmploy):
+    leader = models.ForeignKey(Directors, on_delete=models.PROTECT)
+
+
+class Managers(AbstractEmploy):
+    leader = models.ForeignKey(AbstractEmploy, on_delete=models.PROTECT)
+
+
+class OperatorsKTZ(AbstractEmploy):
+    leader = models.ForeignKey(Managers, on_delete=models.PROTECT)
+
+
+class OperatorsElec(AbstractEmploy):
+    leader = models.ForeignKey(Managers, on_delete=models.PROTECT)
+
+
+class Crawlers(AbstractEmploy):
+    leader = models.ForeignKey(OperatorsKTZ, on_delete=models.PROTECT)
+
+
+class Electric(AbstractEmploy):
+    leader = models.ForeignKey(OperatorsElec, on_delete=models.PROTECT)
+
