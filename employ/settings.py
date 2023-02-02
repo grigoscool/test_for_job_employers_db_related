c4e0ca7419a7e7e1c645bac58ebef20f7c8461be
Sergey Grigorev <grigoscool@mail.ru>
2023-02-02 09:47:04 +0300

    2

--- employ/settings.py
+++ employ/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = Path(BASE_DIR/'work_site/static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- work_site/views.py
+++ work_site/views.py
@@ -4,7 +4,10 @@ from .models import Director, AssociateDir, Manager, OperatorsKTZ, OperatorsElec
 
 def home(request):
     directors = Director.objects.filter(pk__range=(1, 4))
+    asis = AssociateDir.objects.filter(pk__range=(1, 4))
+
     context = {
         'directors': directors,
+        'asis': asis,
     }
     return render(request, 'work_site/index.html', context)
